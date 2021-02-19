f7baa886fb69e773247f820cf30de2f69099bf30
Speloquin <speloqu24@gmail.com>
2021-02-18 18:35:52 -0700

    handleSort

--- src/components/employeeList.js
+++ src/components/employeeList.js
@@ -5,15 +5,12 @@ import SearchEmployee from "./SearchEmployee";
 function EmployeeList() {
   const [search, setSearch] = useState("");
   const [employees, setEmployees] = useState([]);
+
+  // STORES state for filtered employees
   const [filterEmployees, setFilterEmployees] = useState([]);
-
-  //   const { name, phoneNumber, email, birthday } = search;
-
-  // TODO: API
 
   useEffect(() => {
     API.getEmployee().then((res) => {
-      console.log(res.data.results);
       setEmployees(res.data.results);
       setFilterEmployees(res.data.results);
     });
@@ -22,7 +19,7 @@ function EmployeeList() {
   const handleInputChange = (event) => {
     setSearch(event.target.value);
 
-    // filter through employees array here
+    // FILTER through employees array
     const result = employees.filter(
       (employee) =>
         // FILTER by first name
@@ -44,9 +41,11 @@ function EmployeeList() {
 
   // TODO: what is up with the SORT?! research
   const handleSort = () => {
-    employees.sort((a, b) => (a.name.first > b.name.first ? 1 : -1));
-    console.log(employees);
-    setFilterEmployees(employees);
+    const sort = employees.sort((a, b) =>
+      a.name.first > b.name.first ? 1 : -1
+    );
+    setFilterEmployees(sort);
+    console.log(filterEmployees);
   };
 
   return (
@@ -58,16 +57,16 @@ function EmployeeList() {
         <thead>
           <tr>
             <th scope="col"></th>
-            <th scope="col" onClick={handleSort}>
+            <th scope="col" onClick={() => handleSort()}>
               Name
             </th>
             <th scope="col">Phone</th>
-            <th scope="col"> Email</th>
+            <th scope="col">Email</th>
             <th scope="col">DOB</th>
           </tr>
         </thead>
         <tbody>
-          {console.log(filterEmployees)}
+          {/* {console.log(filterEmployees)} */}
           {filterEmployees.map((employee) => (
             <tr key={employee.login.uuid}>
               <td>
